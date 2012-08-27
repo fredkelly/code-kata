a9810a7d8c76bfb12e61724a553731ad09f934dc
Fred Kelly <me@fredkelly.net>
2012-08-27 10:03:46 +0100

    Completed Kata four.

--- kata_four/munger.rb
+++ kata_four/munger.rb
@@ -1,23 +1,46 @@
-data = IO.read('./weather.dat')
-lines = data[%r{<pre>([^<]+)</pre>}].split("\n")
+data = IO.read('./football.dat')
+lines = data[%r{<pre>([^<]+)</pre>}].gsub('-', '').split("\n")
 
-rows = lines[5..-2].map(&:split); labels = lines[3].split
+#rows = lines[5..-2].map(&:split); labels = lines[3].split
+#
+#days = {}
+#rows.each do |row|
+#  days[row.first] = {}
+#  row.each_with_index do |val, i|
+#    days[row.first][labels[i].to_sym] = val.to_f
+#  end
+#end
+#
+#min = 10000; min_no = -1
+#
+#days.each do |day_no, day|
+#  if day[:MxT] - day[:MnT] < min
+#    min = day[:MxT] - day[:MnT]
+#    min_no = day_no
+#  end
+#end
+#
+#puts "Lowest temperature spread #{min} on day #{min_no}."
 
-days = {}
+rows = lines[2..-2].map(&:split); labels = lines[1].split
+
+teams = {}
 rows.each do |row|
-  days[row.first] = {}
-  row.each_with_index do |val, i|
-    days[row.first][labels[i].to_sym] = val.to_f
+  unless row.empty?
+    teams[no=row.delete_at(0).to_i] = {}
+    row.each_with_index do |val, i|
+      teams[no][labels[i].to_sym] = val
+    end
   end
 end
 
-max = 0; max_no = -1
+min = 10000; min_no = -1
 
-days.each do |day_no, day|
-  if day[:MxT] - day[:MnT] > max
-    max = day[:MxT] - day[:MnT]
-    max_no = day_no
+teams.each do |team_no, team|
+  if (delta = (team[:F].to_i - team[:A].to_i).abs) < min
+    min = delta
+    min_no = team_no
   end
 end
 
-puts max_no
+puts "Lowest goal difference #{min} by #{teams[min_no][:Team]}."
