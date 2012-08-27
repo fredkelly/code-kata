094dbfd1e104994c4f2110b8456304ee5ffa4aa0
Fred Kelly <me@fredkelly.net>
2012-08-27 17:00:20 +0100

    Fixed small bug in anagrams.rb.

--- kata_six/anagrams.rb
+++ kata_six/anagrams.rb
@@ -2,23 +2,17 @@ class Anagrams
   def initialize(words)
     @words ||= {}
     IO.foreach(words) do |word|
-      word = word.chop.downcase
-      @words[key = self.class.sort_string(word)] = Array(@words[key]) << word
+      word.chomp!.downcase!
+      @words[key = word.chars.sort.join] = Array(@words[key]) << word
     end
   end
   
-  def found
+  def matched
     @words.select{|k,v| v.size > 1}
-  end
-  
-  private
-  
-  def self.sort_string(string)
-    string.chars.sort.join
   end
 end
 
 if __FILE__ == $0
   anagrams = Anagrams.new(ARGV.first)
-  puts anagrams.found.map{|a| a.join(',')}
+  puts anagrams.matched.map{|a| a.join(',')}
 end
